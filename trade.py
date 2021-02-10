38defda670b439d5248e3a13a46afd47fcb1f009
elevate <3460250+Elevate08@users.noreply.github.com>
2021-02-10 09:28:37 -0500

    limiting options to 30 days or less and api key moved to config.json file

--- trade.py
+++ trade.py
@@ -5,6 +5,7 @@ import requests
 import json
 import argparse
 
+
 def _argparse():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description='Process arguments')
     parser.add_argument('--ticker', type=str, help='Specific ticker to use')
@@ -13,6 +14,7 @@ def _argparse():
 
     args = parser.parse_args()
     return args
+
 
 def get_data(ticker, key):
     # Collect stock data from tdapi from a specific ticker
@@ -36,16 +38,19 @@ def get_options(ticker, key):
                     params={'apikey': key})
     options_data = json.loads(page.content)
 
-    for data in options_data['callExpDateMap']:
-        for d in data:
-            print(d)
+    for date, value in options_data['callExpDateMap'].items():
+        date_array = date.split(":")
+        if int(date_array[1]) <= 30:
+            for key,data in value.items():
+                print(key)
             exit()
+    exit()
 
     for k,v in options_data.items():
         if k == 'daysToExpiration' and v <= 30:
             if k == 'inTheMoney' and v == True:
                 continue
-        
+
 
         '''
             if data['inTheMoney'] == True and d['putCall'] == 'CALL' and d['daysToExpiration'] <= 30:
@@ -60,8 +65,12 @@ def get_options(ticker, key):
 def main():
     args = _argparse()
 
-    key = "KHTCA8CYOAJO5NRROMHJSLWJRIQE4GYP"
-    
+    with open('config.json', 'r') as myfile:
+        data = myfile.read()
+    config = json.loads(data)
+
+    key = config['key']
+
     if args.ticker and args.options == True:
         options_data = get_options(args.ticker, key)
         for k,v in options_data:
