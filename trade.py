879f2ff96279de91f47ae15d712c5b76bf632c25
David <3460250+Elevate08@users.noreply.github.com>
2021-02-10 10:44:29 -0500

    Limit options that are displayed (#2)
    * limiting options to 30 days or less and api key moved to config.json file
    
    * #1 Displaying 'CALL' only and first 3 inTheMoney True and first 3 inTheMoney false

--- trade.py
+++ trade.py
@@ -5,6 +5,7 @@ import requests
 import json
 import argparse
 
+
 def _argparse():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description='Process arguments')
     parser.add_argument('--ticker', type=str, help='Specific ticker to use')
@@ -13,6 +14,7 @@ def _argparse():
 
     args = parser.parse_args()
     return args
+
 
 def get_data(ticker, key):
     # Collect stock data from tdapi from a specific ticker
@@ -35,41 +37,39 @@ def get_options(ticker, key):
     page = requests.get(url=full_url,
                     params={'apikey': key})
     options_data = json.loads(page.content)
-    print(options_data['callExpDateMap'].keys())
-    exit()
-    
-    for data in options_data['callExpDateMap']:
-        print(data)
-        exit()
-        for d in data:
-            print(d)
-            exit()
 
-    for k,v in options_data.items():
-        if k == 'daysToExpiration' and v <= 30:
-            if k == 'inTheMoney' and v == True:
-                continue
-        
+    result = []
+    for date, value in options_data['callExpDateMap'].items():
+        inTM = 0
+        ninTM = 0
+        date_array = date.split(":")
+        if int(date_array[1]) <= 30:
+            for key, data in value.items():
+                for d in data:
+                    if 'CALL' == d['putCall']:
+                        if d['inTheMoney'] is True and inTM != 3:
+                            result.append(d)
+                            inTM += 1
+                        elif d['inTheMoney'] is False and ninTM != 3:
+                            result.append(d)
+                            ninTM += 1
 
-        '''
-            if data['inTheMoney'] == True and d['putCall'] == 'CALL' and d['daysToExpiration'] <= 30:
-                print(d['description'] + '/t' + d['last'])
-        '''
-
-    exit()
-
-    return options_data
+    return result
 
 
 def main():
     args = _argparse()
 
-    key = "KHTCA8CYOAJO5NRROMHJSLWJRIQE4GYP"
-    
+    with open('config.json', 'r') as myfile:
+        data = myfile.read()
+    config = json.loads(data)
+
+    key = config['key']
+
     if args.ticker and args.options == True:
-        options_data = get_options(args.ticker, key)
-        for k,v in options_data:
-            print(k,v)
+        options = get_options(args.ticker, key)
+        for option in options:
+            print(option)
     elif args.ticker:
         ticker_data = get_data(args.ticker, key)
         for k,v in ticker_data:
