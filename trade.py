ee097fcadd071beca4c4cf1bb0644867a250be8d
Daniel Adams <adamsd1019@gmail.com>
2021-02-09 22:15:47 -0500

    get_data function working properly

--- trade.py
+++ trade.py
@@ -1,6 +1,5 @@
 '''
-    Authorize to ToS api
-    Place new limit order
+    Program to collect data and place trades
 '''
 import requests
 import json
@@ -8,12 +7,17 @@ import argparse
 
 def _argparse():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description='Process arguments')
-    parser.add_argument('--ticker', help='Specific ticker to use')
+    parser.add_argument('--ticker', type=str, help='Specific ticker to use')
 
     args = parser.parse_args()
     return args
 
-def get_data(ticker, page):
+def get_data(ticker, endpoint, key):
+    # Collect data from tdapi from a specific ticker
+    # ticker_data returns as a dictionary
+    full_url = endpoint.format(stock_ticker=ticker)
+    page = requests.get(url=full_url,
+                    params={'apikey': key})
     content = json.loads(page.content)
     ticker_data = content[ticker].items()
 
@@ -21,14 +25,14 @@ def get_data(ticker, page):
 
 
 def main():
+    args = _argparse()
+
     key = "KHTCA8CYOAJO5NRROMHJSLWJRIQE4GYP"
     endpoint = 'https://api.tdameritrade.com/v1/marketdata/{stock_ticker}/quotes?'
-    full_url = endpoint.format(stock_ticker='AAL')
-    page = requests.get(url=full_url,
-                    params={'apikey': key})
     
-    ticker_data = get_data(args.ticker, page)
-    print(ticker_data)
+    ticker_data = get_data(args.ticker, endpoint, key)
+    for k,v in ticker_data:
+        print(k,v)
 
     exit()
 
