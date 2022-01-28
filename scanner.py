a473970862dbc8fd13e9f21dd93548b5dc8683cf
Daniel Adams <adamsd1019@gmail.com>
2022-01-28 09:29:56 -0500

    added capability to take in watchlist text file and return current data on all tickers

--- scanner.py
+++ scanner.py
@@ -5,9 +5,9 @@ import requests
 def _argparse():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description='Process arguments')
-    parser.add_argument('-t', '--ticker', type=str, help='Specific ticker to use')
+    parser.add_argument('-t', '--ticker', default=False, help='Specific ticker to use')
     parser.add_argument('-o','--options', action='store_true', help='Toggle for options chain')
-    parser.add_argument('--freq', type=int, help='How many minutes per candle')
+    parser.add_argument('-wl', '--watchlist', action='store_true', help='Check tickers on watchlist')
     args = parser.parse_args()
     return args
 
@@ -21,19 +21,28 @@ def get_data(ticker, key):
     # Collect stock data from tdapi from a specific ticker
     # ticker_data returns as a dictionary
     # returns last one minute candle data
-    stock_endpoint = 'https://api.tdameritrade.com/v1/marketdata/{stock_ticker}/quotes?'
-    full_url = stock_endpoint.format(stock_ticker=ticker)
-    page = requests.get(url=full_url,
-                    params={'apikey': key})
+    if ',' in ticker:
+        stock_endpoint = 'https://api.tdameritrade.com/v1/marketdata/quotes?symbol=' + ticker
+    else:
+        stock_endpoint = f'https://api.tdameritrade.com/v1/marketdata/{ticker}/quotes?'
+    page = requests.get(url=stock_endpoint,
+                        params={'apikey': key})
     content = json.loads(page.content)
-    return content[ticker]
+    return content
 
 def main():
     args = _argparse()
     filepath = 'config.json'
     key = readConfig(filepath)
-    print(get_data(args.ticker, key))
-
+    if args.ticker:
+        print(get_data(args.ticker, key))
+    elif args.watchlist:
+        with open('watchlist.txt', 'r') as file:
+            watchlist = file.readlines()
+            new_watchlist = []
+            for ticker in watchlist:
+                new_watchlist.append(ticker.strip())
+            print(get_data(str(new_watchlist).replace('[', '').replace(']', '').replace("'", '').replace(' ', ''), key))
     exit()
 
 if __name__ == '__main__':
